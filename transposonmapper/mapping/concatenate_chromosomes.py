8307dc573b70bf8d539c204e66f74fc20f898c0f
Leila Inigo de la Cruz <leilaicruz@gmail.com>
2021-07-14 18:09:46 +0200

    adding docstring to add_chromosome_lengths

--- transposonmapper/mapping/concatenate_chromosomes.py
+++ transposonmapper/mapping/concatenate_chromosomes.py
@@ -1,9 +1,7 @@
 import numpy as np
 
 def add_chromosome_length_inserts(coordinates, ref_names, chr_lengths):
-    """
-
-    """
+    
     ll = 0
     for ii in range(1,len(ref_names)):
         ll += chr_lengths[ref_names[ii-1]]
@@ -14,9 +12,31 @@ def add_chromosome_length_inserts(coordinates, ref_names, chr_lengths):
 
 
 def add_chromosome_length(coordinates, chr_lengths_cumsum, ref_tid_roman):
-    """
+    
+    """This function returns a dictionary that for every gene , there is the chromosome number
+    information of where the gene belongs to , the coordinates for the start position, the end position
+    and the direction of the gene. 
+    Parameters
+    ----------
+    coordinates : dict 
+        This dictionary is the 1st output from the function read_genes(gff_file, essentials_file, gene_names_file)
+    chr_lengths_cumsum : dict
+        This dictionary is the 2nd output from the function get_sequence_length(bam)
+    ref_tid_roman : dict
+        This dictionary is the output from 
+        ref_tid_roman = {key: value for key, value in zip(ref_romannums, ref_tid)}, where
+        ref_romannums = chromosomename_roman_to_arabic()[1] and 
+        ref_tid = get_chromosome_names(bam)
+        
 
-    """
+    Returns
+    -------
+    coordinates: dict
+        A dictionary that for every gene , there is the chromosome number
+    information of where the gene belongs to , the coordinates for the start position, the end position
+    and the direction of the gene. 
+    """  
+
     for key in coordinates:
         gene_chrom = ref_tid_roman.get(coordinates.get(key)[0])
         coordinates[key][1] = coordinates.get(key)[1] + chr_lengths_cumsum.get(gene_chrom)
