bb7fc95e290a8c2e8f4977adfef0a6d9ad03648c
Leila Inigo de la Cruz <leilaicruz@gmail.com>
2021-07-16 11:00:56 +0200

    adding docstring

--- transposonmapper/exporting/save_as_bed.py
+++ transposonmapper/exporting/save_as_bed.py
@@ -2,8 +2,21 @@ import os
 
 
 def save_as_bed(file, tncoordinates_array, ref_tid, readnumb_array):
-    """
+    """This function writes in a .bed file located in the same folder as the bamfile, the information abot how many transposons
+    were found in each genomic location. 
 
+    Parameters
+    ----------
+    file : str
+        Path with the filename extension included(e.g "data_file/bedfile_name.bed") describing where do you want to store the results.
+        By default it will be stored in the same location as the bamfile, with the same basename. 
+        Example, if the bamfile path is data_file/data_1.bam then the bed file will be data_file/data_1.bed 
+    tncoordinates_array : numpy array 
+        Second Output from the get_reads.py function : _,tncoordinates_array,_=get_reads(bam)
+    ref_tid : dict 
+        Output from the get_chromosome_names function : ref_tid = get_chromosome_names(bam)
+    readnumb_array : numpy array 
+        1st Output from the get_reads.py function : readnumb_array,_,_=get_reads(bam)
     """
 
     with open(file, "w") as f:
