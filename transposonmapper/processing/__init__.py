34e7349c0182bafa17a9ab750e56af7f09ba23b2
Leila Inigo de la Cruz <leilaicruz@gmail.com>
2021-07-23 13:22:38 +0200

    adding tests and updating init files

---
+++ transposonmapper/processing/__init__.py
@@ -0,0 +1,16 @@
+from .chromosome_names_in_files import chromosome_name_bedfile, chromosome_name_wigfile
+from .clean_bedwigfiles import cleanfiles
+from .essential_genes_names import list_known_essentials
+from profileplot_genome_helpers import (summed_chr,counts_genome,length_genome,middle_chrom_pos,
+binned_list)
+
+
+__all__ = [
+    "chromosome_name_bedfile",
+    "chromosome_name_wigfile",
+    "cleanfiles",
+    "list_known_essentials",
+    "summed_chr","counts_genome",
+    "length_genome","middle_chrom_pos",
+"binned_list"
+]
--- transposonmapper/properties/__init__.py
+++ transposonmapper/properties/__init__.py
@@ -4,11 +4,13 @@ from .get_chromosome_properties import (
     get_sequence_length,
     get_chromosome_reads,
 )
+from.list_gene_names import (list_gene_names)
 
 __all__ = [
     "get_coordinates_genes",
     "get_chromosome_names",
     "get_sequence_length",
     "get_chromosome_reads",
+    "list_gene_names"
 ]
 
